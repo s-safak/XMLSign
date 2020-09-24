6c5bdb84c0a7205b7fedadf7be470febab56e9fc
s-safak <ssafak@gmail.com>
2020-09-25 00:50:58 +0300

    Add files via upload

---
+++ XML_Sign/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XML_Sign
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //D:\S3SDK.3.8\S3AppLib\srcutils
+        }
+    }
+}
